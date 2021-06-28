e22cf4930a8161ed312e197133b68dd53c524f34
unknown <keruiduo@gmail.com>
2021-06-28 12:39:35 +0800

    yop

--- R/run.R
+++ R/run.R
@@ -1,4 +1,21 @@
-launchApp <- function() {
-  print(i18n)
-  #shinyApp(ui = shinyAppUI, server = shinyAppServer)
+run_app <- function(
+  onStart = NULL,
+  options = list(),
+  enableBookmarking = NULL,
+  uiPattern = "/",
+  ...
+) {
+  with_golem_options(
+    app = shinyApp(
+      ui = shinyAppUI,
+      server = shinyAppServer,
+      onStart = onStart,
+      options = options,
+      enableBookmarking = enableBookmarking,
+      uiPattern = uiPattern
+    ),
+    # Initiate the translator as a golem.option
+    #golem_opts = list(translator = shiny.i18n::Translator$new(translation_csvs_path = "inst/app/www/translations/"))
+    golem_opts = list(translator = i18n)
+  )
 }
